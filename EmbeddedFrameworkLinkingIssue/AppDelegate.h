c20dfcce9b4d0b309ccae03fa328fed7e14593d7
Scott Robertson <spr@scottr.org>
2015-10-17 17:45:01 -0700

    Initial Commit

---
+++ EmbeddedFrameworkLinkingIssue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmbeddedFrameworkLinkingIssue
+//
+//  Created by Scott Robertson on 10/17/15.
+//  Copyright © 2015 Scott Paul Robertson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmbeddedFrameworkLinkingIssue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmbeddedFrameworkLinkingIssue
+//
+//  Created by Scott Robertson on 10/17/15.
+//  Copyright © 2015 Scott Paul Robertson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
